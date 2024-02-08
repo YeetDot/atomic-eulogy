811df606afdf9bac2f86db4b34efba95124071d1
Minhao Chen <randomrepl@gmail.com>
2024-02-08 16:10:59 -0500

    Item

--- src/main/java/net/yeetdot/atomiceulogy/AtomicEulogy.java
+++ src/main/java/net/yeetdot/atomiceulogy/AtomicEulogy.java
@@ -2,15 +2,18 @@ package net.yeetdot.atomiceulogy;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.yeetdot.atomiceulogy.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AtomicEulogy implements ModInitializer {
-	public static final string MOD_ID = "atomiceulogy";
+	public static final String MOD_ID = "atomiceulogy";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
+
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/yeetdot/atomiceulogy/AtomicEulogyClient.java
@@ -0,0 +1,12 @@
+package net.yeetdot.atomiceulogy;
+
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AtomicEulogyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
